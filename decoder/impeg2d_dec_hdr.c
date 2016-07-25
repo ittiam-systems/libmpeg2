212b4c4179975c1a263d5ee4723223181a8b46ad
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2016-07-25 11:25:25 +0530

    Initialize sequence display extension fields to signal unspecified values
    Bug: 29640760
    Change-Id: I7cf99d432035576471e188b2da5328f91b7a9a81

--- decoder/impeg2d_dec_hdr.c
+++ decoder/impeg2d_dec_hdr.c
@@ -378,6 +378,9 @@ void impeg2d_dec_seq_disp_ext(dec_state_t *ps_dec)
     impeg2d_bit_stream_get(ps_stream, 4);
     ps_dec->u1_video_format = impeg2d_bit_stream_get(ps_stream, 3);
     ps_dec->u1_colour_description = impeg2d_bit_stream_get(ps_stream, 1);
+    ps_dec->u1_colour_primaries = 2;
+    ps_dec->u1_transfer_characteristics = 2;
+    ps_dec->u1_matrix_coefficients = 2;
     if(ps_dec->u1_colour_description)
     {
         ps_dec->u1_colour_primaries = impeg2d_bit_stream_get(ps_stream, 8);
@@ -390,6 +393,7 @@ void impeg2d_dec_seq_disp_ext(dec_state_t *ps_dec)
     GET_MARKER_BIT(ps_dec,ps_stream);
     ps_dec->u2_display_vertical_size   = impeg2d_bit_stream_get(ps_stream,14);
 
+    ps_dec->u1_seq_disp_extn_present = 1;
     impeg2d_next_start_code(ps_dec);
 }
 
