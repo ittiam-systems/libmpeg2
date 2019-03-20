4c747e2e9afa6a4852be37f97d17d9a19d8fea34
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2019-03-19 20:30:36 -0700

    Reset buffer managers during reset control call
    In some cases, some picture buffer entries in buffer manager were not
    released during reset.
    Added a function to reset buffer manager entries and called that during
    reset control call.
    Without this in some call sequences Decoder would run out of picture
    buffers and not produce correct output.
    
    Bug: 124531341
    Test: atest android.media.cts.DecoderTest
    Change-Id: I75e4422263b0a3e66807376567705b1f06e89cff

--- decoder/impeg2d_api_main.c
+++ decoder/impeg2d_api_main.c
@@ -992,26 +992,11 @@ IV_API_CALL_STATUS_T impeg2d_api_reset(iv_obj_t *ps_dechdl,
 
     if(ps_dec_state_multi_core != NULL)
     {
-        if(ps_dec_state->aps_ref_pics[1] != NULL)
-            impeg2_buf_mgr_release(ps_dec_state->pv_pic_buf_mg, ps_dec_state->aps_ref_pics[1]->i4_buf_id, BUF_MGR_REF);
-        if(ps_dec_state->aps_ref_pics[0] != NULL)
-            impeg2_buf_mgr_release(ps_dec_state->pv_pic_buf_mg, ps_dec_state->aps_ref_pics[0]->i4_buf_id, BUF_MGR_REF);
-        while(1)
-        {
-            pic_buf_t *ps_disp_pic = impeg2_disp_mgr_get(&ps_dec_state->s_disp_mgr, &ps_dec_state->i4_disp_buf_id);
-            if(NULL == ps_disp_pic)
-                break;
-            if(0 == ps_dec_state->u4_share_disp_buf)
-                impeg2_buf_mgr_release(ps_dec_state->pv_pic_buf_mg, ps_disp_pic->i4_buf_id, BUF_MGR_DISP);
-
-        }
-
-        if((ps_dec_state->u4_deinterlace) && (NULL != ps_dec_state->ps_deint_pic))
-        {
-            impeg2_buf_mgr_release(ps_dec_state->pv_pic_buf_mg,
-                                   ps_dec_state->ps_deint_pic->i4_buf_id,
-                                   MPEG2_BUF_MGR_DEINT);
-        }
+        impeg2_buf_mgr_reset(ps_dec_state->pv_pic_buf_mg);
+        /* Display buffer manager init behaves like a reset
+         * as it doesn't need to preserve picture buffer addresses
+         * like buffer manager */
+        impeg2_disp_mgr_init(&ps_dec_state->s_disp_mgr);
 
         for(i4_num_threads = 0; i4_num_threads < MAX_THREADS; i4_num_threads++)
         {
