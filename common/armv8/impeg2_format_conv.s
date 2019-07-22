6f9b564b9e407e401bc2e6c1c24ff50a3ca00fde
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2019-07-22 14:40:52 -0700

    Add support for handling odd dimensions
    Test: poc in bug
    Bug: 137798382
    
    Change-Id: I9df6b869eeb9857799789397615dcb264f73614f

--- common/armv8/impeg2_format_conv.s
+++ common/armv8/impeg2_format_conv.s
@@ -186,6 +186,10 @@ yuv420sp_uv_chroma:
 
     ldr             w8, [sp, #104]      //// Load u2_dest_stride_uv from stack
     sxtw            x8, w8
+    add             x6, x6, 1
+    bic             x6, x6, #1
+
+    add             x9, x9, 1
 
     sub             x7, x7, x6, lsr #1  //// Source increment
 
@@ -360,6 +364,11 @@ yuv420sp_vu_chroma:
 
     ldr             w8, [sp, #104]      //// Load u2_dest_stride_uv from stack
     sxtw            x8, w8
+
+    add             x6, x6, 1
+    bic             x6, x6, #1
+
+    add             x9, x9, 1
 
     sub             x7, x7, x6, lsr #1  //// Source increment
 
